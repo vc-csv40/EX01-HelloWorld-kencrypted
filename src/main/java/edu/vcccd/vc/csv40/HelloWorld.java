5fa73b9c71597edf8733bbc2a91d0938742107ac
vscilab <a@b.com>
2020-02-03 14:15:24 -0800

    Hello

---
+++ src/main/java/edu/vcccd/vc/csv40/HelloWorld.java
@@ -0,0 +1,16 @@
+package edu.vcccd.vc.csv40;
+
+/**
+ * CS V40 Beginning Java
+ * Assignment: HelloWorld
+ *
+ * Statement of code ownership: I hereby state that I have written all of this
+ * code and I have not copied this code from any other person or source.
+ *
+ * @author [Kenneth Lawrence]
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
